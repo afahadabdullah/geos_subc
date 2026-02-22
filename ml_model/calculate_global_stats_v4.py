2d578cacd6721708cfc54fdc1a686b417b5b84a8
afahadabdullah <a.fahad@nasa.gov>
2026-02-22 15:36:01 -0500

    Hardcode target data directory absolute path to dataprocess directory on node

--- ml_model/calculate_global_stats_v4.py
+++ ml_model/calculate_global_stats_v4.py
@@ -95,7 +95,7 @@ def calculate_global_stats(data_root, out_path="v4_global_stats.pt", batch_size=
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_root", type=str, default="/scratch/11353/afahad/geossub/data")
+    parser.add_argument("--data_root", type=str, default="/home1/11353/afahad/geos_subc/dataprocess")
     parser.add_argument("--out", type=str, default="ml_model/v4_global_stats.pt")
     args = parser.parse_args()
     
