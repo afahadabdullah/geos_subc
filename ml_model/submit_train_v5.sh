fd6663f00acd5d72f5d83ec9dc3bc60001a4c69f
afahadabdullah <a.fahad@nasa.gov>
2026-03-04 10:52:52 -0500

    Merge stdout+stderr into single .log file for train v5

--- ml_model/submit_train_v5.sh
+++ ml_model/submit_train_v5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J flow_v5                   # Job name
-#SBATCH -o ml_output_flow5/flow_%j.out
-#SBATCH -e ml_output_flow5/flow_%j.err
+#SBATCH -o ml_output_flow5/flow_%j.log
+#SBATCH -j y                          # Merge stderr into stdout
 #SBATCH -p gh-dev                    # Queue (partition) name
 #SBATCH -N 1                         # Total # of nodes
 #SBATCH -n 1                         # Total # of tasks
