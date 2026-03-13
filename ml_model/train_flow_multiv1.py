Fix variance target to be mean-normalized relative spatial multiplier

ml_model/train_flow_multiv1.py
@@ -1049,8 +1049,10 @@ def train(args, accelerator):
             loss_vel = (area_weights * land_ocean_weights * temp_weights_expanded * (v_pred - v_target)**2).mean()
 
             # 2. Variance MSE Loss
-            # Target variance is proxy'd as the squared error of the current velocity prediction
-            var_target = (v_target - v_pred.detach())**2
+            # We want the variance head to predict the RELATIVE spatial scaling of the error,
+            # so we normalize the absolute error by its spatial mean before squaring.
+            abs_err = torch.abs(v_target - v_pred.detach())
+            var_target = (abs_err / (abs_err.mean(dim=(2, 3), keepdim=True) + 1e-6))**2
             loss_var = (area_weights * land_ocean_weights * temp_weights_expanded * (var_pred - var_target)**2).mean()
 
             # 3. Weighted total loss (0.9 / 0.1 split)
@@ -1216,7 +1218,8 @@ def train(args, accelerator):
                     temp_weights = w_escalation[lead_idx].view(B, 1, 1, 1).expand(-1, 2, -1, -1)
                     
                     loss_vel = (area_weights * land_ocean_weights * temp_weights * (v_pred - v_target)**2).mean()
-                    var_target = (v_target - v_pred.detach())**2
+                    abs_err = torch.abs(v_target - v_pred.detach())
+                    var_target = (abs_err / (abs_err.mean(dim=(2, 3), keepdim=True) + 1e-6))**2
                     loss_var = (area_weights * land_ocean_weights * temp_weights * (var_pred - var_target)**2).mean()
                     
                     loss_val = 0.9 * loss_vel + 0.1 * loss_var